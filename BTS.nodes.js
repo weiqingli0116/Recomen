62a252554f3d0a03eb61c478dff1cb8f4adb05b9
yingnan han <yingnanhan@autoreg-130674.dyn.wpi.edu>
2018-04-24 23:40:41 -0400

    update cluster and demo
    update cluster and demo

---
+++ BTS.nodes.js
@@ -0,0 +1,180 @@
+var nodes = [{
+  id: "Z32", text: "0.077 sauce chicken salad tomato bbq ordered potato", data: { name: "Z32", level: 3, percentage: 0.077 }, children: [{
+      id: "Z217", text: "0.052 sauce tomato bbq potatoes dip mac-cheese baked", data: { name: "Z217", level: 2, percentage: 0.052 }, children: [{
+          id: "Z163", text: "0.025 sauce tomato bbq potatoes dip mac-cheese baked", data: { name: "Z163", level: 1, percentage: 0.025 }, children: []
+        }, {
+          id: "Z164", text: "0.009 spinach mushrooms roasted pepper flavor shared rich", data: { name: "Z164", level: 1, percentage: 0.009 }, children: []
+        }]
+    }, {
+      id: "Z216", text: "0.022 hot perfectly thai cooked cheese pad grilled", data: { name: "Z216", level: 2, percentage: 0.022 }, children: [{
+          id: "Z154", text: "0.037 perfectly cooked cheese grilled bacon meat veggies", data: { name: "Z154", level: 1, percentage: 0.037 }, children: []
+        }, {
+          id: "Z153", text: "0.039 hot dogs chocolate white disappointing start finish", data: { name: "Z153", level: 1, percentage: 0.039 }, children: []
+        }, {
+          id: "Z152", text: "0.005 thai pad lettuce craving joint indian sandwich", data: { name: "Z152", level: 1, percentage: 0.005 }, children: []
+        }, {
+          id: "Z158", text: "0.002 rib prime sports worked option plan company", data: { name: "Z158", level: 1, percentage: 0.002 }, children: []
+        }, {
+          id: "Z155", text: "0.004 medium rare space makes wonderful job fantastic", data: { name: "Z155", level: 1, percentage: 0.004 }, children: []
+        }, {
+          id: "Z156", text: "0.028 husband date evening daughter loved years-ago ended", data: { name: "Z156", level: 1, percentage: 0.028 }, children: []
+        }, {
+          id: "Z160", text: "0.003 miss hit eat feel inside felt places", data: { name: "Z160", level: 1, percentage: 0.003 }, children: []
+        }, {
+          id: "Z161", text: "0.016 valley green beans good delicious neighborhood looked", data: { name: "Z161", level: 1, percentage: 0.016 }, children: []
+        }, {
+          id: "Z157", text: "0.010 hidden gem multiple credit doctor times outdoor", data: { name: "Z157", level: 1, percentage: 0.01 }, children: []
+        }, {
+          id: "Z159", text: "0.003 south located west asian finished eating starbucks", data: { name: "Z159", level: 1, percentage: 0.003 }, children: []
+        }, {
+          id: "Z162", text: "0.036 establishment hummus spring texture break taste dish", data: { name: "Z162", level: 1, percentage: 0.036 }, children: []
+        }]
+    }, {
+      id: "Z218", text: "0.043 salad ordered potato fries soup french pork", data: { name: "Z218", level: 2, percentage: 0.043 }, children: [{
+          id: "Z166", text: "0.045 salad ordered potato fries soup french side", data: { name: "Z166", level: 1, percentage: 0.045 }, children: []
+        }, {
+          id: "Z165", text: "0.035 crispy onion calamari add perfect pretty-good treated", data: { name: "Z165", level: 1, percentage: 0.035 }, children: []
+        }, {
+          id: "Z167", text: "0.003 pork belly slices salmon steak thin duck", data: { name: "Z167", level: 1, percentage: 0.003 }, children: []
+        }, {
+          id: "Z168", text: "0.002 oil general chili change enjoyed spice choice", data: { name: "Z168", level: 1, percentage: 0.002 }, children: []
+        }, {
+          id: "Z169", text: "0.012 reason art main real traditional kinda wall", data: { name: "Z169", level: 1, percentage: 0.012 }, children: []
+        }]
+    }, {
+      id: "Z29", text: "0.039 chicken fried dry wings crab juicy pieces", data: { name: "Z29", level: 2, percentage: 0.039 }, children: [{
+          id: "Z129", text: "0.033 chicken fried dry wings eggs cold time", data: { name: "Z129", level: 1, percentage: 0.033 }, children: []
+        }, {
+          id: "Z128", text: "0.025 crab juicy pieces served wrap cake sausage", data: { name: "Z128", level: 1, percentage: 0.025 }, children: []
+        }]
+    }, {
+      id: "Z210", text: "0.038 bite grab quick size burger portion pay", data: { name: "Z210", level: 2, percentage: 0.038 }, children: [{
+          id: "Z130", text: "0.003 bite grab quick move charged fill fixed", data: { name: "Z130", level: 1, percentage: 0.003 }, children: []
+        }, {
+          id: "Z131", text: "0.026 pay car price wash fair cash additional", data: { name: "Z131", level: 1, percentage: 0.026 }, children: []
+        }, {
+          id: "Z132", text: "0.007 burger veggie forget product rolls mention scottsdale", data: { name: "Z132", level: 1, percentage: 0.007 }, children: []
+        }, {
+          id: "Z133", text: "0.009 size portion nice regular appetizer cocktail enjoy", data: { name: "Z133", level: 1, percentage: 0.009 }, children: []
+        }]
+    }, {
+      id: "Z215", text: "0.009 ago months weeks card customer-service gift excellent", data: { name: "Z215", level: 2, percentage: 0.009 }, children: [{
+          id: "Z147", text: "0.011 ago months weeks card gift charge friend", data: { name: "Z147", level: 1, percentage: 0.011 }, children: []
+        }, {
+          id: "Z149", text: "0.053 customer-service excellent friendly-staff matter month hands supposed", data: { name: "Z149", level: 1, percentage: 0.053 }, children: []
+        }, {
+          id: "Z148", text: "0.026 cashier discount baby seats extremely cleaning wife", data: { name: "Z148", level: 1, percentage: 0.026 }, children: []
+        }, {
+          id: "Z150", text: "0.038 disappointed forward employees incredibly couple incredible girls", data: { name: "Z150", level: 1, percentage: 0.038 }, children: []
+        }, {
+          id: "Z151", text: "0.003 chips salsa free offer called front-desk buy", data: { name: "Z151", level: 1, percentage: 0.003 }, children: []
+        }]
+    }, {
+      id: "Z28", text: "0.017 reviews yelp reading place glad found town", data: { name: "Z28", level: 2, percentage: 0.017 }, children: [{
+          id: "Z126", text: "0.007 reviews yelp reading checked checking website shops", data: { name: "Z126", level: 1, percentage: 0.007 }, children: []
+        }, {
+          id: "Z127", text: "0.060 place found glad town worth totally recommend", data: { name: "Z127", level: 1, percentage: 0.06 }, children: []
+        }]
+    }, {
+      id: "Z213", text: "0.018 floor dance front door open air opened", data: { name: "Z213", level: 2, percentage: 0.018 }, children: [{
+          id: "Z142", text: "0.008 floor dance worse garlic corn fresh tuna", data: { name: "Z142", level: 1, percentage: 0.008 }, children: []
+        }, {
+          id: "Z141", text: "0.034 front door open opened air house box", data: { name: "Z141", level: 1, percentage: 0.034 }, children: []
+        }, {
+          id: "Z143", text: "0.039 party super-friendly entire terrible wedding recently forever", data: { name: "Z143", level: 1, percentage: 0.039 }, children: []
+        }, {
+          id: "Z144", text: "0.035 make drive park reservation home store leave", data: { name: "Z144", level: 1, percentage: 0.035 }, children: []
+        }]
+    }, {
+      id: "Z211", text: "0.005 hour half returning future massage booked picked", data: { name: "Z211", level: 2, percentage: 0.005 }, children: [{
+          id: "Z135", text: "0.031 future returning massage booked picked normal quality-food", data: { name: "Z135", level: 1, percentage: 0.031 }, children: []
+        }, {
+          id: "Z134", text: "0.011 hour half hours reservations early check drinks", data: { name: "Z134", level: 1, percentage: 0.011 }, children: []
+        }, {
+          id: "Z136", text: "0.001 heavy deep greasy crazy offered rush work", data: { name: "Z136", level: 1, percentage: 0.001 }, children: []
+        }]
+    }, {
+      id: "Z214", text: "0.033 prices reasonable high quality cheap school broth", data: { name: "Z214", level: 2, percentage: 0.033 }, children: [{
+          id: "Z146", text: "0.019 prices reasonable cheap fast", data: { name: "Z146", level: 1, percentage: 0.019 }, children: []
+        }, {
+          id: "Z145", text: "0.005 high quality school broth pho year ice-cream", data: { name: "Z145", level: 1, percentage: 0.005 }, children: []
+        }]
+    }, {
+      id: "Z212", text: "0.039 deal big find back call stores products", data: { name: "Z212", level: 2, percentage: 0.039 }, children: [{
+          id: "Z138", text: "0.032 deal big find stores products cheaper difficult", data: { name: "Z138", level: 1, percentage: 0.032 }, children: []
+        }, {
+          id: "Z137", text: "0.009 back call purchase ahead moved walk corner", data: { name: "Z137", level: 1, percentage: 0.009 }, children: []
+        }, {
+          id: "Z139", text: "0.010 salads sandwiches attitude positive station kind weird", data: { name: "Z139", level: 1, percentage: 0.01 }, children: []
+        }, {
+          id: "Z140", text: "0.030 pour les des great area selection beer", data: { name: "Z140", level: 1, percentage: 0.03 }, children: []
+        }]
+    }]
+}, {
+  id: "Z31", text: "0.009 tea iced flavors milk coffee combo chinese", data: { name: "Z31", level: 3, percentage: 0.009 }, children: [{
+      id: "Z21", text: "0.018 flavors combo style pizzas avocado tomatoes mixed", data: { name: "Z21", level: 2, percentage: 0.018 }, children: [{
+          id: "Z11", text: "0.002 combo style pizzas street excited surprised pricing", data: { name: "Z11", level: 1, percentage: 0.002 }, children: []
+        }, {
+          id: "Z12", text: "0.034 flavors avocado tomatoes lemon mixed list top", data: { name: "Z12", level: 1, percentage: 0.034 }, children: []
+        }]
+    }, {
+      id: "Z22", text: "0.008 tea iced milk coffee chinese mexican large", data: { name: "Z22", level: 2, percentage: 0.008 }, children: [{
+          id: "Z14", text: "0.008 tea iced milk coffee chinese mexican large", data: { name: "Z14", level: 1, percentage: 0.008 }, children: []
+        }, {
+          id: "Z13", text: "0.014 team bed story management bathroom tasting playing", data: { name: "Z13", level: 1, percentage: 0.014 }, children: []
+        }]
+    }, {
+      id: "Z27", text: "0.041 classic downtown american phoenix italian rate donuts", data: { name: "Z27", level: 2, percentage: 0.041 }, children: [{
+          id: "Z116", text: "0.011 classic american italian rate basic heart casual", data: { name: "Z116", level: 1, percentage: 0.011 }, children: []
+        }, {
+          id: "Z115", text: "0.009 downtown phoenix donuts world staff-friendly middle word", data: { name: "Z115", level: 1, percentage: 0.009 }, children: []
+        }, {
+          id: "Z117", text: "0.064 people talk shop crowd noodle vibe write", data: { name: "Z117", level: 1, percentage: 0.064 }, children: []
+        }, {
+          id: "Z120", text: "0.048 years nail past salon lived taking lady", data: { name: "Z120", level: 1, percentage: 0.048 }, children: []
+        }, {
+          id: "Z122", text: "0.014 night saturday dinner strip service bad review", data: { name: "Z122", level: 1, percentage: 0.014 }, children: []
+        }, {
+          id: "Z119", text: "0.005 shopping center started show provided level request", data: { name: "Z119", level: 1, percentage: 0.005 }, children: []
+        }, {
+          id: "Z124", text: "0.011 walked immediately hotel stayed sit guests crowded", data: { name: "Z124", level: 1, percentage: 0.011 }, children: []
+        }, {
+          id: "Z125", text: "0.019 salty bit beef read onions lamb tasty", data: { name: "Z125", level: 1, percentage: 0.019 }, children: []
+        }, {
+          id: "Z118", text: "0.013 drink pulled orders juice orange brisket hold", data: { name: "Z118", level: 1, percentage: 0.013 }, children: []
+        }, {
+          id: "Z121", text: "0.002 turn great-service great-food serving servers provide employee", data: { name: "Z121", level: 1, percentage: 0.002 }, children: []
+        }, {
+          id: "Z123", text: "0.030 sunday brunch afternoon friday busy portions generous", data: { name: "Z123", level: 1, percentage: 0.03 }, children: []
+        }]
+    }, {
+      id: "Z24", text: "0.052 attentive server questions asked friendly answer staff", data: { name: "Z24", level: 2, percentage: 0.052 }, children: [{
+          id: "Z19", text: "0.047 attentive questions server asked friendly answer staff", data: { name: "Z19", level: 1, percentage: 0.047 }, children: []
+        }, {
+          id: "Z18", text: "0.119 remember shot warm missing bucks slightly purchased", data: { name: "Z18", level: 1, percentage: 0.119 }, children: []
+        }, {
+          id: "Z110", text: "0.021 woman skin cream working cookies rude color", data: { name: "Z110", level: 1, percentage: 0.021 }, children: []
+        }]
+    }, {
+      id: "Z25", text: "0.033 stars give giving rating chance left hang", data: { name: "Z25", level: 2, percentage: 0.033 }, children: [{
+          id: "Z112", text: "0.025 stars give giving chance left waitress customers", data: { name: "Z112", level: 1, percentage: 0.025 }, children: []
+        }, {
+          id: "Z111", text: "0.129 rating hang sort airport games und gravy", data: { name: "Z111", level: 1, percentage: 0.129 }, children: []
+        }]
+    }, {
+      id: "Z26", text: "0.028 hard soft choose rock variety parking-lot helped", data: { name: "Z26", level: 2, percentage: 0.028 }, children: [{
+          id: "Z114", text: "0.031 hard soft choose variety rock parking-lot serve", data: { name: "Z114", level: 1, percentage: 0.031 }, children: []
+        }, {
+          id: "Z113", text: "0.016 helped today stopped city decor works mind", data: { name: "Z113", level: 1, percentage: 0.016 }, children: []
+        }]
+    }, {
+      id: "Z23", text: "0.086 noticed minutes wait waited watching line long", data: { name: "Z23", level: 2, percentage: 0.086 }, children: [{
+          id: "Z16", text: "0.020 noticed watching guy walking sitting game typical", data: { name: "Z16", level: 1, percentage: 0.02 }, children: []
+        }, {
+          id: "Z15", text: "0.066 minutes wait waited long line sat bottom", data: { name: "Z15", level: 1, percentage: 0.066 }, children: []
+        }, {
+          id: "Z17", text: "0.024 table arrived healthy week offers rest greeted", data: { name: "Z17", level: 1, percentage: 0.024 }, children: []
+        }]
+    }]
+}
+];
